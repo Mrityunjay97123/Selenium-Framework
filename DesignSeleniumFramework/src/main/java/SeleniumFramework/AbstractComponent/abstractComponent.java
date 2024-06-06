15320c18e320e205ad8a3fc33be8bf9dd332c2ae
Mrityunjay Dwivedee <mrityunjaydw.pd@gmail.com>
2024-06-06 10:42:10 +0530

    Initial commit

---
+++ DesignSeleniumFramework/src/main/java/SeleniumFramework/AbstractComponent/abstractComponent.java
@@ -0,0 +1,52 @@
+package SeleniumFramework.AbstractComponent;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import SeleniumFramework.PageObjectModel.CartPage;
+
+public class abstractComponent {
+	
+	WebDriver driver;
+	
+	public abstractComponent(WebDriver driver) {
+		
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@FindBy(xpath = "(//*[@class='btn btn-custom'])[3]")
+	WebElement cartHeader;
+	
+	
+	
+	public CartPage goToCartPage()
+	{
+		cartHeader.click();
+		
+		CartPage cartpage = new CartPage(driver);
+		return cartpage;
+	}
+
+	public void waitForElementToAppear(By findBy)
+	{
+	WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+	wait.until(ExpectedConditions.visibilityOfElementLocated(findBy));
+	}
+	
+	public void waitForElementToDisappear(WebElement Ele)
+	{
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.invisibilityOf(Ele));
+	}
+
+}
